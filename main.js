998e2906579adf32f59147d1adc777f40da0cd39
Sam Wheeling <anchormansam@gmail.com>
2019-09-06 21:03:04 -0400

    maybe

--- main.js
+++ main.js
@@ -39,8 +39,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    var weight = Math.round(300 * .07) + 'stone';
-    var temperature =  Math.round((94 - 32) * .71) + 'Centigrade';
+    var weight = Math.round(300 * .07) + ' stone';
+    var temperature =  Math.round((94 - 32) * .71) + ' Centigrade';
     newStory=newStory.replace('94 fahrenheit',temperature)
     newStory=newStory.replace('300 pounds', weight)
 
